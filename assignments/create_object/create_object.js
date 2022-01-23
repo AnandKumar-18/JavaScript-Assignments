cbbfa98c5228f9d8ec6d8f9f179af342958f7ecf
Anand Kumar <anandkc589@gmail.com>
2022-01-23 10:54:17 +0530

    my-sixth-assignment
    Signed-off-by: Anand Kumar <anandkc589@gmail.com>

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,13 @@
 function CreateObject(arr) {
     // Write your code here
+    var new_arr = {};
+    //console.log(arr.length)
+    i=0
+    while (i< arr.length){
+    new_arr[arr[i]] = arr[i+1];
+    i+=2
+    }
+    return new_arr;
 }
 
 module.exports = CreateObject;
